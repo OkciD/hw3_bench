d3cd0c260da1295076ca104fca2f8fd000263314
Yury Baykov <y.baykov@corp.mail.ru>
2020-01-20 10:52:32 +0300

    parsing jsons into structs

--- fast.go
+++ fast.go
@@ -1,10 +1,42 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"hw3_bench/structs"
 	"io"
+	"os"
 )
 
 // вам надо написать более быструю оптимальную этой функции
 func FastSearch(out io.Writer) {
-	SlowSearch(out)
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		userJsonBytes := scanner.Bytes()
+		user := structs.User{}
+
+		err := user.UnmarshalJSON(userJsonBytes)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Fprintln(out, user)
+	}
+
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
+}
+
+func main() {
+	FastSearch(os.Stdout)
 }
