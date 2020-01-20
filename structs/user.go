e5c4291763e7177034cf0e049ab3208d947c7e3f
Yury Baykov <y.baykov@corp.mail.ru>
2020-01-20 10:19:40 +0300

    easyjson codegen

---
+++ structs/user.go
@@ -0,0 +1,12 @@
+package structs
+
+//easyjson:json
+type User struct {
+	Browsers []string
+	Company  string
+	Country  string
+	Email    string
+	Job      string
+	Name     string
+	Phone    string
+}
