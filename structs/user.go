26e1bfc346eb9337f5e01fab70c5999fe17d5810
Yury Baykov <y.baykov@corp.mail.ru>
2020-01-22 11:07:06 +0300

    optimized structure

--- structs/user.go
+++ structs/user.go
@@ -3,10 +3,10 @@ package structs
 //easyjson:json
 type User struct {
 	Browsers []string `json:"browsers"`
-	Company  string   `json:"company"`
-	Country  string   `json:"country"`
-	Email    string   `json:"email"`
-	Job      string   `json:"job"`
-	Name     string   `json:"name"`
-	Phone    string   `json:"phone"`
+	//Company  string   `json:"company"`
+	//Country  string   `json:"country"`
+	Email string `json:"email"`
+	//Job      string   `json:"job"`
+	Name string `json:"name"`
+	//Phone    string   `json:"phone"`
 }
